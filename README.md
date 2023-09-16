d58312e58456fd2d0a88950284ee2fb745409d99
meitlpey <prmeitler@mail.fhsu.edu>
2023-09-16 11:23:01 -0500

    Added "How to run" to the read me file

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Peyton Meitler
 This project will be using the packages NumPy and Matplotlib in order to create 5 graphs that output as PNG files.
 ## pip install instruction
 please run the following
-'''
+```
 pip install -r -requirments.txt
-'''
+```
+
+## How to run
+In a terminal window please type the following:
+```
+python main.py
+```
--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 # Peyton Meitler
 # Mini Project 1
 
-#(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-#(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 #(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 #(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
 
